ee4b6cefc2f1164a667fad54505815cacb1aa647
Ciryl Krauchuk <nissan.cyril@gmail.com>
2022-11-14 19:38:31 +0300

    Lesson 1.4 ready

---
+++ 1.4/SkillBox1.4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillBox1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*Console.WriteLine:*");
+            Console.WriteLine("Hello world!!!");
+            Console.WriteLine();
+            Console.WriteLine("*Console.Write:*");
+            Console.Write("Hello ");
+            Console.Write("world");
+            Console.Write("!!!");
+
+            Console.ReadKey();
+        }
+    }
+}
