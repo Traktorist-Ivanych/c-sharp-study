7122aeaf0e1b693e78374ab5e59ca291341f14fa
Ciryl Krauchuk <nissan.cyril@gmail.com>
2022-11-15 20:51:53 +0300

    lesson 2.7 ready

---
+++ 2.7/SkillBox2_7/SkillBox2_7/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillBox2_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullName;
+            string Surname = "Кравчук";
+            string Name = "Кирилл";
+            string Patronymic = "Игоревич";
+            fullName = "Фамилия :{0} \nИмя: {1} \nОтчество: {2}";
+            Console.WriteLine(fullName, Surname, Name, Patronymic);
+
+            int age = 23;
+            Console.WriteLine("Возраст: " + age.ToString());
+
+            string email = "nissan.cyril@gmail.com";
+            Console.WriteLine("Email: " + email);
+
+            string scoresOutput = "Набранные баллы:\nПрограммирование - {0} \nМатематика - {1} \nФизика - {2}";
+            double programmingScores = 79.1;
+            double mathScores = 99.9;
+            double physicsScores = 92.2;
+            Console.WriteLine(scoresOutput, programmingScores, mathScores, physicsScores);
+
+            Console.ReadKey();
+
+            Console.WriteLine();
+
+            double scoresSum = programmingScores + mathScores + physicsScores;
+            Console.WriteLine("Сумма набранных баллов: " + scoresSum.ToString());
+            double scoreAvarage = Math.Round(scoresSum / 3, 1, MidpointRounding.AwayFromZero);
+            Console.WriteLine("Среднее арифметическое набранных баллов: " + scoreAvarage.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2.7/SkillBox2_7/SkillBox2_7/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("SkillBox2_7")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SkillBox2_7")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("99e39e2f-8704-4013-b9b7-f9f7748ff274")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
